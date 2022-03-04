36329d949be7fc697be037df3b56e4e4fcf46d5b
agou <2833769658@qq.com>
2022-03-05 01:55:03 +0800

    [Upd] 针对首页等的多项修改

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,19 @@
-// 这是只在首页执行的 js
+var cardSwiper = new Swiper('.cardSwiper',
+    {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 'auto',
+        centeredSlides: false,
+        slidesOffsetAfter: 0,
+        pagination: { el: '.swiper-pagination', },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        navigation:
+            { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev', },
+    });
 
-window.onloadstart = function () {
-    window.location.href = "#";
-}
-
-var mySwiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 'auto',
-    loopedSlides: 2,
-    centeredSlides: true,
-    autoplay: {
-        delay: 3000,
-        stopOnLastSlide: false,
-        disableOnInteraction: true,
-    },
-
-    // 如果需要分页器
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // 如果需要前进后退按钮
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // 如果需要滚动条
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-})
 
 // header icon 
 
@@ -45,3 +27,38 @@ function setHeader() {
 }
 
 setHeader();
+
+// index Newsroom Article
+
+loadNews();
+
+function loadNews() {
+    $("#newsSwiperItems").html(`<center><span data-i18n="index.newslist_loading"><span></center>`);
+    changeLanguage();
+    $.ajax("https://newsroom.nmteam.xyz/api/posts?showContent=false&pageSize=5&page=1", {
+        type: "GET",
+        async: true,
+        data: {},
+        crossDomain: true,
+        datatype: "jsonp",
+        success: function (data) {
+            newsHTML = ``;
+            if (data['status'] == 200) {
+                data['data'].forEach(function (value) {
+                    newsHTML += `<a class="swiper-slide" href="${value['permalink']}" target="_blank" title="${value['title']}">
+                        <h3>${value['title']}</h3>
+                        <div class="time"><svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M512 1024C229.229714 1024 0 794.770286 0 512S229.229714 0 512 0s512 229.229714 512 512-229.229714 512-512 512z m0-73.142857c242.358857 0 438.857143-196.498286 438.857143-438.857143S754.358857 73.142857 512 73.142857 73.142857 269.641143 73.142857 512s196.498286 438.857143 438.857143 438.857143z m36.571429-694.857143v256.365714l255.963428-0.365714 0.073143 73.142857-329.179429 0.475429V256h73.142858z" p-id="3478"></path></svg>${value['year']}-${value['month']}-${value['day']}</div>
+                        <p>${value['text'].split("<!--more-->")[0].replace(/\n/g, "<br />").replace(/#/g, "").replace(/![img](S*)/g, "")}</p>
+                        <div class="cover"></div>
+                    </a>`;
+                });
+            }
+            $("#newsSwiperItems").html(newsHTML);
+            cardSwiper[1].updateSlides();
+        },
+        error: function () {
+            $("#newsSwiperItems").html(`<center><span data-i18n="index.newslist_error"></span><div class="indexMoreLinks"><a href="javascript:" onclick="loadNews();" data-button-type="no"><span data-i18n="index.retry"></span></a></div></center>`);
+            changeLanguage();
+        }
+    });
+}
