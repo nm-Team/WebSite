f93b02b13bc6c7ed4948cd701633933a108117c7
agoudbg <2833769658@qq.com>
2022-07-04 22:24:46 +0800

    [Add] 同步 nmTeam 加入问卷

--- join/forum.php
+++ join/forum.php
@@ -1,21 +1,2 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/functions.php");
-define("page_title", "join.join_register_page.title");
-define("page_keywords", "欢迎你,加入nmTeam,招人,招贤纳财");
-define("page_description", "join.join_register_page.description");
-define("page_head_css", array("/src/css/join.css"));
-define("page_head_js", array());
-define("page_body_js", array("/src/js/join.js"));
-define("page_image", "");
-define("page_update", "20220331");
-setHeader();
-?>
-<div class="title center">
-    <h1><?php p("join.join_register_page.title"); ?></h1>
-</div>
-<div class="main">
-    <div class="mainBlock">
-        <p><?php p("join.join_register_page.tip"); ?></p>
-    </div>
-</div>
-<?php setFooter(); ?>
+header("Location: /blackboard/questionnaire/22_07_04_join_nmteam_".$_GET['jobType']);
