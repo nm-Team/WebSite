208a8a5b03edd7e8f02ceb3cd874732bbdc5b0d8
agoudbg <agoudbg@gmail.com>
2023-05-12 00:14:24 +0800

    chore: 转到注脚链接本地化

--- src/js/products.js
+++ src/js/products.js
@@ -1,6 +1,7 @@
 productsTargetInI18n = productI18n.target_in_aria_label;
 productsTargetOutI18n = productI18n.target_out_aria_label;
 productsTargetDetailI18n = productI18n.target_detail_aria_label;
+productsTargetTurnToNoteI18n = productI18n.target_turn_to_note;
 
 svgList = {
     "in": `<svg class="svg" aria-label="${productsTargetInI18n}" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M761.056 532.128c0.512-0.992 1.344-1.824 1.792-2.848 8.8-18.304 5.92-40.704-9.664-55.424L399.936 139.744c-19.264-18.208-49.632-17.344-67.872 1.888-18.208 19.264-17.376 49.632 1.888 67.872l316.96 299.84-315.712 304.288c-19.072 18.4-19.648 48.768-1.248 67.872 9.408 9.792 21.984 14.688 34.56 14.688 12 0 24-4.48 33.312-13.44l350.048-337.376c0.672-0.672 0.928-1.6 1.6-2.304 0.512-0.48 1.056-0.832 1.568-1.344C757.76 538.88 759.2 535.392 761.056 532.128z" p-id="2301"></path></svg>`,
@@ -18,7 +19,7 @@ function checkTip(w, json) {
             tipsNum++;
             products_footer_explain_by_javascript.innerHTML += `<p id="products_footer_explain_` + tipsNum + `"><b>` + tipsNum + `. </b>` + json['tip'][sTipsNum] + `</p>`;
             sTipsNum++;
-            return `<object><a class="tip" href="#products_footer_explain_` + tipsNum + `" target="_self" title="Turn to note">` + tipsNum + `</a></object>`;
+            return `<object><a class="tip" href="#products_footer_explain_` + tipsNum + `" target="_self" title="${productsTargetTurnToNoteI18n}">` + tipsNum + `</a></object>`;
         });
         if (w && chinesePun.indexOf(w[w.length - 1]) > 0) {
             w = w.slice(0, -1) + "<bs>" + w[w.length - 1] + "</bs>";
