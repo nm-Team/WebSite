f8ac046fc74ebc94b094bb2f0fb8fd70425bc1cc
agoudbg <agoudbg@gmail.com>
2026-08-11 17:36:01 +0800

    chore: remove legacy PHP website assets (#28)

--- src/js/tab.js
+++
@@ -1,8 +0,0 @@
-$(".tabHeader button").on("click", function(){
-    focusTab($(this).parent().parent(),$(this).attr("data-tab"));
-})
-
-function focusTab(ele, tabid) {
-    ele.find("*").attr("data-status", "");
-    ele.find("*[data-tab=" + tabid + "]").attr("data-status", "focus");
-}
